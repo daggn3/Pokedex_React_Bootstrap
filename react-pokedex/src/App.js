683468df84c8df979c5f109a6f97039cdb21816a
Niall Dagg <daggbros@gmail.com>
2021-03-29 12:50:20 +0100

    intial work

--- react-pokedex/src/App.js
+++ react-pokedex/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 
-function App() {
+// Pages
+import Homepage from './pages/Homepage';
+import PokemonPage from './pages/Pokemonpage';
+
+// Components
+import Header from './components/Header';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header/>
+      <Container>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/pokemon/:id' component={PokemonPage}/>
+      </Container>
+    </Router>
   );
 }
 
---
+++ react-pokedex/src/components/Pokemon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const pokemon = ({ pokemon }) => {
+
+    return (
+        <>
+            <Card className='my-3 p-3 rounded text-center shadow p-3 mb-5 bg-white rounded' style={{ border: 'none' }}>
+                <Link to={`/pokemon/${pokemon.id}`}>
+                    <Card.Img style={{ width: '8rem' }} src={pokemon.sprites.front_default} variant='top'/>
+                </Link>
+                <Card.Body className={`${pokemon.types[0].type.name} rounded text-white`}>
+                    <Link to={`/pokemon/${pokemon.name}`} className='link-name'>
+                        <Card.Title as='div'>#{pokemon.id} : {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</Card.Title>
+                    </Link>
+                </Card.Body>
+            </Card>
+        </>
+    )
+}
+
+export default pokemon;
