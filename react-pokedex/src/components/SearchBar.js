3b4d0c4b81942137d064f4575779047a2bae4692
Niall Dagg <daggbros@gmail.com>
2021-03-31 22:20:21 +0100

    updated home button

--- react-pokedex/src/components/SearchBar.js
+++ react-pokedex/src/components/SearchBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { useHistory } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import { HouseFill } from 'react-bootstrap-icons';
 
 const SearchBar = ({input:keyword, onChange:setKeyword}) => {
 
@@ -14,11 +15,11 @@ const SearchBar = ({input:keyword, onChange:setKeyword}) => {
     
 
 
-  const BarStyling = { opacity: "0.7", marginLeft: "auto", marginTop: "10px", fontSize: "20px", fontWeight: "300", color: "#333", lineHeight: "1.625", backgroundColor: "#fff", borderColor: "#eee", border: "none", borderBottom: "1px solid #9e9e9e", boxShadow: "none", boxSizing: "content-box", borderRadius: "0", backgroundImage: "url( //addsearch.com/logo/AAAAAA-20.png )", backgroundRepeat: "no-repeat", backgroundPosition: "95% 50%", width: "33%", padding: "5px", cursor: "auto", outline: "none" };
+  const BarStyling = {marginTop: "10px", fontSize: "16px", fontWeight: "300", lineHeight: "1.625", backgroundColor: "#eee", backgroundImage: "url(//addsearch.com/logo/AAAAAA-20.png)", backgroundRepeat: "no-repeat", backgroundPosition: "95% 50%", border: "0px", borderRadius: "32px", width: "33%", padding: "15px", cursor:"auto", outline: "none"};
   return (
     
       <div>
-          <Button style = {{marginBottom: "5px", backgroundColor:"#09bdb7", border:"none"  }} onClick={handleClick}>Return Home</Button>{' '}
+          <Button style = {{ marginBottom: "5px", backgroundColor:"#09bdb7", border:"none"  }} onClick={handleClick}> < HouseFill size={30} /></Button>{' '}
     <input 
      style={BarStyling}
      key="random1"
