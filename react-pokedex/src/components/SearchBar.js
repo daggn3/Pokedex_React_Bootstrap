aa5dc43cd686b4da561437c727e173f3b01e7648
Niall Dagg <daggbros@gmail.com>
2021-03-31 13:13:22 +0100

    added stylings to search

--- react-pokedex/src/components/SearchBar.js
+++ react-pokedex/src/components/SearchBar.js
@@ -3,10 +3,9 @@ import { Search } from 'react-bootstrap-icons';
 
 const SearchBar = ({input:keyword, onChange:setKeyword}) => {
 
-  const BarStyling = {marginTop: "20px", width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
+  const BarStyling = {marginTop: "10px", fontSize: "16px", fontWeight: "300", color: "#333", lineHeight: "1.625", backgroundColor: "#fff", borderColor: "#eee", border: "none", borderBottom: "1px solid #9e9e9e", boxShadow: "none", boxSizing: "content-box", borderRadius: "0", backgroundImage: "url( //addsearch.com/logo/AAAAAA-20.png )", backgroundRepeat: "no-repeat", backgroundPosition: "95% 50%", width: "35%", padding: "5px", cursor: "auto", outline: "none" };
   return (
       <div>
-      <Search size={20} style= {{marginRight: "5px"}}/>
     <input 
      style={BarStyling}
      key="random1"
